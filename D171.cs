1366de6ace2a200ffb4e62d3d4ea3cee16b13965
Peter Shen <minus190710@163.com>
2021-07-31 21:52:38 +0800

    Daily exercise 171

---
+++ D171.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace DailyExercise
+{
+    // 171. Excel表列序号
+    public class D171
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int multiply = 1;
+            int ans = 0;
+            for (int i = columnTitle.Length - 1; i >= 0; i--)
+            {
+                ans += (columnTitle[i] - 64) * multiply;
+                multiply *= 26;
+            }
+            return ans;
+        }
+    }
+    
+    // const int asc = 'A' - 64;
+}
