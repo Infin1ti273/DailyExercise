e2597e231ce7724ff9a206ac5c708835863bdd1b
Peter Shen <minus190710@163.com>
2021-08-06 21:30:48 +0800

    Daily 847 Failed

---
+++ Daily_Failed/D847.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+//847. 访问所有节点的最短路径 (需要了解动态规划)
+namespace DailyExercise.Daily_Failed
+{
+    public class D847
+    {
+        public int ShortestPathLength(int[][] graph)
+        {
+            const int inf = int.MaxValue / 2;
+            int n = graph.Length;
+            Queue<>
+            for (int i = 0; i < n; i++)
+            {
+                int[] record = new int[n];
+                Array.Fill(record, inf);
+                record[i] = 
+
+            }
+
+        }
+    }
+}
