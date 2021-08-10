9dbb5be67fa04f0b8d2aaa4042b880036b88399e
Peter Shen <minus190710@163.com>
2021-08-10 19:51:25 +0800

    Daily 413

---
+++ Daily_Cleared/D413.cs
@@ -0,0 +1,34 @@
+
+namespace DailyExercise.Daily_Cleared
+{
+    public class D413
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+            int n = nums.Length;
+            int ans = 0;
+            int ptr = 0;
+            if (n==1)
+            {
+                return 0;
+            }
+            int variance = nums[ptr + 1] - nums[ptr];
+            for (int i = 1; i < n;i++)
+            {
+                if (i-ptr<2)
+                    continue;
+                if ((i-ptr)*variance==(nums[i]-nums[ptr]))
+                {
+                    ans += (i - ptr - 1);
+                }
+                else
+                {
+                    ptr = i - 1;
+                    variance = nums[ptr + 1] - nums[ptr];
+                }
+            }
+            return ans;
+        }
+        
+    }
+}
